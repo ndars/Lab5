1e7e6a77115457466af2f96bd14a8dfaba18f8c8
ndars <89598232+ndars@users.noreply.github.com>
2022-03-03 11:53:42 -0500

    Added try/catch for CreditValue, fixed type mismatch in constructor calls

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -10,29 +10,22 @@ package com.mycompany.csc325_oop_designreview_lab;
  */
 public class MainClass {
 	
-	public static void main(String[] args) {		                
-                // Todo 7: Create two classes for Freshman and Senior 
-                
-                // ToDo 8: The senior class should have a minimum of 85 credits  
+	public static void main(String[] args) {		                                
+            Student std1= new Freshman("James", (short)20, 12); // name, age, credits
+            Student std2;
+            try {
+                std2 = new Senior("John", (short)30, 90);
+            } catch (Senior.CreditValueException ex) {
+                std2 = new Freshman("John", (short)30, 90); // instantiates a freshman instead
+            }
 		
-		// ToDo 9: Add a toString method for Freshman class
-		
-                // ToDo 10: Add a toString method for Senior class
-                
-		Student std1= new Freshman("James", 20, 12); // name, age, credits
-                
-                Student std2 = new Senior("John", 30, 90);
-		
-		// ToDo 11: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
-		
-		System.out.println(std1);
-                
-                System.out.println(std2);
-		
-		// ToDo 12: add comments and explain your code
-		
-		// ToDo 13: submit using a pull request.
+            System.out.println(std1);
+
+            System.out.println(std2);
+
+            // ToDo 12: add comments and explain your code
+
+            // ToDo 13: submit using a pull request.
 	}
 
 }
