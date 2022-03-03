a29e3e3c3d9e1de934dd61b6a8d639d353a0175b
ndars <89598232+ndars@users.noreply.github.com>
2022-03-03 11:33:17 -0500

    Fixed instantiation error

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -6,15 +6,11 @@ package com.mycompany.csc325_oop_designreview_lab;
 
 /**
  *
- * @author MoaathAlrajab
+ * @author MoaathAlrajab, ndars
  */
 public class MainClass {
 	
-	public static void main(String[] args) {
-		// ToDo 5: Fix the error
-		
-		// ToDo 6: Fix the constructor of Student class
-                
+	public static void main(String[] args) {		                
                 // Todo 7: Create two classes for Freshman and Senior 
                 
                 // ToDo 8: The senior class should have a minimum of 85 credits  
@@ -23,9 +19,9 @@ public class MainClass {
 		
                 // ToDo 10: Add a toString method for Senior class
                 
-		Freshman std1= new Student("James", 20, 12); // name, age, credits
+		Student std1= new Freshman("James", 20, 12); // name, age, credits
                 
-                Senior std2 = new Student("John", 30, 90);
+                Student std2 = new Senior("John", 30, 90);
 		
 		// ToDo 11: Set the gpa of the student using the scanner and user
 		// 			input and then print the output.
